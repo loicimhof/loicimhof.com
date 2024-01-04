47978a8432ef8ee97536e998345d3b3d7f36796b
loicimhof <imhof.loic04@gmail.com>
2024-01-04 15:03:38 +0100

    wip: styled about component a bit

--- loicimhof/src/components/About.tsx
+++ loicimhof/src/components/About.tsx
@@ -1,52 +1,73 @@
-import React from "react";
-import { Card, Grid, Text, Title, Image, Button, GridCol } from "@mantine/core";
+import {
+  Card,
+  Grid,
+  Text,
+  Title,
+  Image,
+  Button,
+  GridCol,
+  Box,
+} from "@mantine/core";
 
 const About = () => {
   return (
-    <Grid gutter="lg">
-      <GridCol span={6} style={{ display: "flex", justifyContent: "center" }}>
-        <Image
-          src="../passfoto.jpg"
-          alt="Photo"
-          style={{ height: "400px", width: "300px", borderRadius: 15 }}
-        />
-      </GridCol>
-      <GridCol span={6}>
-        <Card shadow="xs" padding="lg">
-          <Title order={2}>About Me</Title>
+    <Card
+      shadow="xs"
+      padding="lg"
+      style={{ width: "80%", boxShadow: "0 0 0 1px teal ", borderRadius: 15 }}
+    >
+      <Grid gutter="lg">
+        <GridCol span={6} style={{ display: "flex", justifyContent: "center" }}>
+          <Image
+            src="../passfoto.jpg"
+            alt="Photo"
+            style={{ height: "400px", width: "300px", borderRadius: 15 }}
+          />
+        </GridCol>
+        <GridCol span={6}>
+          <Title order={2} style={{ margin: 20 }}>
+            About Me
+          </Title>
           <Text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae
-            elit libero, a pharetra augue. Sed non mauris vitae erat consequat
-            auctor eu in elit. Class aptent taciti sociosqu ad litora torquent
-            per conubia nostra, per inceptos himenaeos.
+            I'm 19 years old and I'm learning software engineering at
+            NoserYoung. I love technology and always want to be up to date with
+            the latest advancements.
           </Text>
-          <Button
-            component="a"
-            href="https://twitter.com"
-            variant="outline"
-            left="twitter"
+          <Box
+            style={{
+              display: "flex",
+              alignContent: "flex-end",
+              height: "100%",
+            }}
           >
-            Twitter
-          </Button>
-          <Button
-            component="a"
-            href="https://github.com"
-            variant="outline"
-            left="github"
-          >
-            GitHub
-          </Button>
-          <Button
-            component="a"
-            href="https://linkedin.com"
-            variant="outline"
-            left="linkedin"
-          >
-            LinkedIn
-          </Button>
-        </Card>
-      </GridCol>
-    </Grid>
+            <Button
+              component="a"
+              href="https://twitter.com"
+              variant="outline"
+              left="twitter"
+            >
+              Twitter
+            </Button>
+            <Button
+              component="a"
+              href="https://github.com"
+              variant="outline"
+              left="github"
+            >
+              GitHub
+            </Button>
+            <Button
+              component="a"
+              href="https://linkedin.com"
+              variant="outline"
+              left="linkedin"
+            >
+              LinkedIn
+            </Button>
+          </Box>
+        </GridCol>
+      </Grid>
+    </Card>
   );
 };
 
